6e7d667c2e67720c4bf426233dfd59f867e3d4bf
Jannchie <jannchie@gmail.com>
2023-06-19 23:52:01 +0900

    :adhesive_bandage: fix: home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home ({ children }: { children: ReactNode } & any) {
   return (
     <>
       <T.H1
-        className="text-center text-7xl from-cyan-600 to-blue-800 gradient-text font-extrabold text-transparent bg-clip-text bg-gradient-to-r "
+        className="text-center text-3xl md:text-5xl lg:text-7xl from-cyan-600 to-blue-800 gradient-text font-extrabold text-transparent bg-clip-text bg-gradient-to-r "
       >
         zeroroku.com
       </T.H1>
